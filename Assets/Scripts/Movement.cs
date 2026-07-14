13daa79d1b8164c49c0a6cd0459dcd0a48f5641c
Tushar280 <chaudarytushar280@gmail.com>
2026-07-14 14:58:04 +0530

    Add auto horizontal movement and tweak scene transform
    Movement.cs: add a serialized controlSpeed field (10f) and an Update() that moves the object along +X by controlSpeed * Time.deltaTime (y forced to 0). OnMove remains unchanged. Assets/Scenes/Lvl1.unity: adjust a Transform's localPosition.z from -12.570007 to -13.55 to tweak scene layout.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,8 +3,19 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private float controlSpeed = 10f;
+    Vector2 movement;
+
+    private void Update()
+    {
+        float xOffset = controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset,0f,0f);
+    }
+
+
     private void OnMove(InputValue value)
     {
         Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
     }
 }
