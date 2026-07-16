83f8e79c79c7b4608276b147c35d6c9260d10d7b
Tushar280 <chaudarytushar280@gmail.com>
2026-07-16 14:01:02 +0530

    Update PlayerWeapon.cs

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -30,6 +30,13 @@ public class PlayerWeapon : MonoBehaviour
         }
     }
 
+    private void RotateToTarget()
+    {
+        Vector3 dir = trackingTarget.position - transform.position;
+        float angle = Vector3.SignedAngle(transform.up, dir, Vector3.forward);
+        transform.Rotate(0, 0, angle);
+    }
+
 
     private void TrackMouseTarget()
     {
