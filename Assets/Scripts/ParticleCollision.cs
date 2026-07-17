ed3e1b2af263b4556a7ffe4d80338a4beff74dce
Tushar280 <chaudarytushar280@gmail.com>
2026-07-17 18:27:08 +0530

    Update ParticleCollision.cs

--- Assets/Scripts/ParticleCollision.cs
+++ Assets/Scripts/ParticleCollision.cs
@@ -4,6 +4,7 @@ public class ParticleCollision : MonoBehaviour
 {
     [SerializeField] private GameObject exploVfx;
     [SerializeField] private int hitPoints = 10;
+    [SerializeField]private ScoreBoard scoreBoard;
 
     private void OnParticleCollision(GameObject other)
     {
