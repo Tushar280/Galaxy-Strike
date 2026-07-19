21c20620f22ce99c9b968c4d3e36464c89015275
Tushar280 <chaudarytushar280@gmail.com>
2026-07-19 22:41:37 +0530

    Update CollisionHandler.cs

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private GameObject exploVfx;
+    private SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindFirstObjectByType<SceneLoader>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Instantiate(exploVfx,transform.position, Quaternion.identity);
         Destroy(gameObject);
-        ReloadScene();
+        sceneLoader.ReloadScene();
     }
 }
