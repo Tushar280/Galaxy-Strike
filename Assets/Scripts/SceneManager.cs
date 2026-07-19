dd143cbb116851669ec2bf9ba1c8b86745e330a7
Tushar280 <chaudarytushar280@gmail.com>
2026-07-19 22:33:52 +0530

    Update SceneManager.cs

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int waitSeconds = 2;
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
