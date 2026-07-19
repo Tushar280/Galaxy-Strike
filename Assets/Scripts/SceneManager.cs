4b5fb510448ec2dbf847c8d1f10f4838153944b1
Tushar280 <chaudarytushar280@gmail.com>
2026-07-19 22:34:27 +0530

    Update SceneManager.cs

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class SceneManager : MonoBehaviour
 {
     [SerializeField] private int waitSeconds = 2;
+
+    public void ReloadScene()
+    {
+        
+    }
     
 
 
